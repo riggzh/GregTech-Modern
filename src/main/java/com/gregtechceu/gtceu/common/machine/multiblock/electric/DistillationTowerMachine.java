501be52d933c2e2deffcd12c38d7ff1b9c712a45
kross <135918757+krossgg@users.noreply.github.com>
2025-01-23 10:15:01 +0800

    Fix merge issues

--- src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/DistillationTowerMachine.java
+++ src/main/java/com/gregtechceu/gtceu/common/machine/multiblock/electric/DistillationTowerMachine.java
@@ -94,28 +94,13 @@ public class DistillationTowerMachine extends WorkableElectricMultiblockMachine
 
                 var part = parts.get(outputIndex);
                 if (part.self().getPos().getY() == y) {
-                    part.getRecipeHandlers().handlerMap.forEach((cap, handler) -> {
-                        if (handler instanceof IFluidHandler fluidHandler) {
-                            fluidOutputs.add(fluidHandler);
-                            if (firstValid == null) {
-                                firstValid = fluidHandler;
-                            }
-                        } else {
-                            fluidOutputs.add(VoidFluidHandler.INSTANCE);
-                        }
-                    });
-
-                    /*
-                     * part.getRecipeHandlers().stream()
-                     * .filter(IFluidHandler.class::isInstance)
-                     * .findFirst()
-                     * .ifPresentOrElse(h -> {
-                     * fluidOutputs.add((IFluidHandler) h);
-                     * if (firstValid == null) firstValid = (IFluidHandler) h;
-                     * },
-                     * () -> fluidOutputs.add(VoidFluidHandler.INSTANCE));
-                     */
-
+                    var handler = part.getRecipeHandlers().getCapability(FluidRecipeCapability.CAP)
+                            .stream()
+                            .filter(IFluidHandler.class::isInstance)
+                            .findFirst()
+                            .map(IFluidHandler.class::cast)
+                            .orElse(VoidFluidHandler.INSTANCE);
+                    addOutput(handler);
                     outputIndex++;
                 } else if (part.self().getPos().getY() > y) {
                     fluidOutputs.add(VoidFluidHandler.INSTANCE);
